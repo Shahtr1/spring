c8eee0643407da188d8fb4bfb2762acedaaf293d
Shahrukh Tramboo <shahrukht2539@arthfc.local>
2020-11-08 23:06:29 +0530

    started XML configuration of spring beans

---
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,48 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	private String emailAddress;
+	private String team;
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
+
+	public CricketCoach() {
+		
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
